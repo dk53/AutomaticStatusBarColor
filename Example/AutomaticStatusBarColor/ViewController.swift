a6acc2de0daee96f96985f69310508e6e25ff41b
Victor Carmouze <victor.carmouze@backelite.com>
2017-02-04 16:38:00 +0100

    Updating exemple App

--- Example/AutomaticStatusBarColor/ViewController.swift
+++ Example/AutomaticStatusBarColor/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        AutomaticStatusBarColor.sharedInstance.force(statusBarStyle: .lightContent, forViewController: self)
+        force(statusBarStyle: .lightContent)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
